e04293cb5a85688e3315dd4263a2625dcf29c6ff
Tajerbashi <KamranTajerbashi@gmail.com>
2022-03-31 06:03:58 +0430

    Pyhon is Lesson One Complete

---
+++ 1-Lesson/HelloWorld.py
@@ -0,0 +1,80 @@
+print("Hello World I am Python Your Best Friend in this time Now & Future !!")
+
+print("=======================================")
+
+x = 1
+if x == 1:
+    # indented four spaces
+    print("x is 1.")
+
+print("=======================================")
+
+myint = 7
+print(myint)
+
+print("=======================================")
+
+myfloat = 7.0
+print(myfloat)
+myfloat = float(7)
+print(myfloat)
+
+print("=======================================")
+
+mystring = 'hello'
+print(mystring)
+mystring = "hello"
+print(mystring)
+
+print("=======================================")
+
+mystring = "Don't worry about apostrophes"
+print(mystring)
+
+print("=======================================")
+
+one = 1
+two = 2
+three = one + two
+print(three)
+
+hello = "hello"
+world = "world"
+helloworld = hello + " " + world
+print(helloworld)
+
+print("=======================================")
+
+a, b = 3, 4
+print(a, b)
+
+print("=======================================")
+print("==========       Error       ==========")
+
+# This will not work!
+one = 1
+two = 2
+hello = "hello"
+
+# print(one + two + hello)
+
+print("=======================================")
+
+# change this code
+mystring = "hello"
+myfloat = 10.0
+myint = 20
+
+# testing code
+if mystring == "hello":
+    print("String: %s" % mystring)
+if isinstance(myfloat, float) and myfloat == 10.0:
+    print("Float: %f" % myfloat)
+if isinstance(myint, int) and myint == 20:
+    print("Integer: %d" % myint)
+
+print("=======================================")
+print("=======================================")
+print("=======================================")
+print("=======================================")
+
